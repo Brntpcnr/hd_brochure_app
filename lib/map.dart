37bf02fdbe588dd2dc75ae44a99610a69c508ac7
Brntpcnr <brntpcnr@gmail.com>
2023-05-24 22:18:28 +0300

    H-D 2023 Brochure App

--- lib/map.dart
+++ lib/map.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'welcome.dart';
 import 'main.dart';
 import 'home.dart';
 
 
-void main() {
-  runApp(const Maps());
-}
-
 class Maps extends StatelessWidget {
-  const Maps({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -30,16 +26,18 @@ class Maps extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
-        child: (
-            Center(
-              child: Padding(
-                padding: EdgeInsets.fromLTRB(5, 15, 5, 0),
-                child: Text("Check if your local H-D office have what you need. Whether it be a Lowrider-S bike or a mini-ape handlebar. We got you.",
-                    textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 18, fontStyle: FontStyle.italic)),
-              ),
-            )
+        child:
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+            child: Text("Check if your local H-D office have what you need. Whether it be a Lowrider-S bike or a mini-ape handlebar. We got you.",
+            textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 18, fontStyle: FontStyle.italic)
+            ),
+          ),
         ),
       ),
     );
   }
 }
+
+
