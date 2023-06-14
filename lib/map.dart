4103434e7e600130118f2ebf96341b15499b41d4
Brntpcnr <brntpcnr@gmail.com>
2023-06-14 17:47:19 +0300

    H-D App

--- lib/map.dart
+++ lib/map.dart
@@ -50,7 +50,7 @@ class _MapsState extends State<Maps> {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        backgroundColor: Colors.deepOrange,
+        backgroundColor: Colors.orange,
         title: Center(
             child:  Padding(
               padding: const EdgeInsets.fromLTRB(0, 0, 50, 0),
